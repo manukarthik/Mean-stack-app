08774a5f01c88a6b974c9a4737cf52b2ccab7d38
manukarthik <manukarthikkr@gmail.com>
2018-03-21 20:41:04 -0400

    addition of mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var cors = require('cors')
+var bodyParser = require('body-parser')
+var mongoose =  require('mongoose')
 var app = express();
+
+var User = require('./models/user.js')
+
 var posts = [
 {
-    message: 'hello'
+    message: 'hell'
 },
 {
     message:'hi'
@@ -11,9 +16,22 @@ var posts = [
 ]
 
 app.use(cors())
+app.use(bodyParser.json())
 app.get('/posts', (req, res) => {
     res.send(posts)
 })
-
-
-app.listen(3000)
+app.post('/register', (req, res) => {
+    var userData = req.body;
+    var user = new User(userData)
+    user.save((err, result)=>{
+        console.log('saving user error')
+        res.sendStatus(200)
+    })
+    console.log(userData)
+    res.sendStatus(200)
+})
+mongoose.connect('mongodb://test:test@ds121589.mlab.com:21589/mean',(err)=>{
+    if(!err)
+    console.log('Connected to mongo')
+})
+app.listen(3000) 
