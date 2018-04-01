fb20e3c67ff99d647a2d6b38ca54e8afa6fefbd7
manukarthik <manukarthikkr@gmail.com>
2018-04-01 14:44:31 -0400

    Creating a auth router

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ var mongoose =  require('mongoose')
 var jwt = require('jwt-simple')
 var app = express();
 var bcrypt= require('bcrypt-nodejs')
-var User = require('./models/user.js')
+var User = require('./models/User.js')
+var auth = require('./auth.js')
 
 var posts = [
 {
@@ -46,38 +47,12 @@ app.get('/profile/:id', async (req, res) => {
     }
 })
 
-app.post('/register', (req, res) => {
-    var userData = req.body;
-    var user = new User(userData)
-    user.save((err, result)=>{
-        console.log('saving user eror')
-        res.sendStatus(200)
-    })
-   
-    res.sendStatus(200)
-})
 
-app.post('/login',async (req, res) => {
-    var LoginData = req.body;
-    var user = await User.findOne({ email: LoginData.email })
-    if(!user)
-      return res.status(401).send({message:'Email or Password invalid'})
-bcrypt.compare(LoginData.password, user.password,(err,isMatch)=>{
-    if (!isMatch)
-        return res.status(401).send({ message: 'Email or passoword invalid' })
-    var payload = {}
 
-    var token = jwt.encode(payload, '123')
-    console.log(token)
-    res.status(200).send({ token })
-})
-    
-      
-})
 mongoose.connect('mongodb://test:test@ds121589.mlab.com:21589/mean',(err)=>{
     if(!err) 
     console.log('Connected to mongo')
 })
 
-
+app.use('/auth', auth)
 app.listen(3000) 
